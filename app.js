da41b5632c5b90729642dc593875c0ff2fb23b33
Morgan Haskins <morgan.haskins@fusionary.com>
2021-07-07 21:54:32 -0400

    Progress so far

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cart = require('./cart-items');
 const app = express();
 const port = 3000;
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
 
 app.use(express.json());
 app.use('/cart-items', cart);
--- cart-items.js
+++ cart-items.js
@@ -3,76 +3,98 @@ const pool = require("./pg-connection-pool");
 const cart = express.Router();
 
 async function getTable(req, res){
-    
+    const results = await pool.query('select * from shopping_cart;')
+    res.json(results.rows);
   }
 
-cart.get("/", (req, res) => {
-    let cart = req.query["cartItems"]
-    console.log("");
+cart.get("/", (req, res)=>{
+    // getTable(req, res);
     let maxPrice = req.query.maxPrice;
-    let returnCart = cartItems;
-    if (maxPrice){
-      returnCart = cartItems.filter((item)=> item.price <= maxPrice)
+    if(maxPrice){
+        pool.query("select * from shopping_cart where price<=$1", [maxPrice]).then(result =>{
+            res.json(result.rows);
+        })
     }
+
     let prefix = req.query.prefix;
     if(prefix){
-      returnCart = returnCart.filter((item)=> item.product.startsWith(prefix))
+        pool.query("select * from shopping_cart where product ilike $1", [prefix + "%"]).then(result =>{
+            res.json(result.rows);
+        })
     }
+
     let pageSize = req.query.pageSize;
     if(pageSize){
-      returnCart = returnCart.slice(0, pageSize);
+        pool.query("select * from shopping_cart limit $1", [pageSize]).then(result =>{
+            res.json(result.rows);
+        })
     }
+    
+});
 
-    res.json(returnCart);
-  });
-
-  cart.get("/:id", (req, res) => {
+cart.get("/:id", async (req, res) =>{
     let id = req.params.id;
-    console.log("Getting ", id);
-    let found = cartItems.find((item) => {
-        return item.id == id
-    })
-    if(found){
+    //if result.row count
+    // if result.row.length
 
-    }else{
-      return res.status(404).send('ID Not Found');
-    }
-    res.json(found);
-  });
+    // let found = await pool.query("SELECT * FROM shopping_cart WHERE id=$1," [id]);
+    // if(found){
+    //     res.json(result.rows);
+    // }else{
+    //     res.status(404).send('ID Not Found');
+    // }
+    // res.json(found);
+});
 
-  cart.post("/", (req, res) => {
-      let test = req.body.test
-      console.log("Test results:", test )
-      let x = cartItems.length + 1;
-      let newItem = req.body;
-      newItem.id = x;
-      cartItems.push(newItem);
-      res.status(201).json(cartItems);
-      res.json("Adding new item..");
-  });
+
+
+//   cart.get("/:id", (req, res) => {
+//     let id = req.params.id;
+//     console.log("Getting ", id);
+//     let found = cartItems.find((item) => {
+//         return item.id == id
+//     })
+//     if(found){
+
+//     }else{
+//       return res.status(404).send('ID Not Found');
+//     }
+//     res.json(found);
+//   });
+
+//   cart.post("/", (req, res) => {
+//       let test = req.body.test
+//       console.log("Test results:", test )
+//       let x = cartItems.length + 1;
+//       let newItem = req.body;
+//       newItem.id = x;
+//       cartItems.push(newItem);
+//       res.status(201).json(cartItems);
+//       res.json("Adding new item..");
+//   });
   
 
-  cart.put("/:id", (req, res) => {
-    let id = req.params.id;
-    let updatedCart = req.body;
-    let found = cartItems.findIndex((item) => item.id == id);
-    if(found){
-      cartItems[found] = {...cartItems[found], ...updatedCart};
-      res.json(cartItems[found]);
-    }else{
-      res.json("No item updates");
+//   cart.put("/:id", (req, res) => {
+//     let id = req.params.id;
+//     let updatedCart = req.body;
+//     let found = cartItems.findIndex((item) => item.id == id);
+//     if(found){
+//       cartItems[found] = {...cartItems[found], ...updatedCart};
+//       res.json(cartItems[found]);
+//     }else{
+//       res.json("No item updates");
 
-    }
+//     }
 
-  });
+//   });
 
-  cart.delete("/:id", (req, res) => {
-      //logic to delete a student
-      let id = req.params.id;
-      let index = cartItems.findIndex(item => item.id == id);
-      cartItems.splice(index, 1);
-      console.log(cartItems)
-      res.status(204).json(cartItems);
-  });
+//   cart.delete("/:id", (req, res) => {
+//       //logic to delete a student
+//       let id = req.params.id;
+//       let index = cartItems.findIndex(item => item.id == id);
+//       cartItems.splice(index, 1);
+//       console.log(cartItems)
+//       res.status(204).json(cartItems);
+//   });
 
   module.exports = cart;
